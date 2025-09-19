6d9d8c82780a67e8797629a29cb4389a500d6520
Nur <aysenur1310@hotmail.com>
2025-09-19 14:48:35 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
 // JavaScript Document
 console.log("hi");
+
+
+// dropdown-menu
+let openMenuButton = document.querySelector("header > button");
+
+openMenuButton.addEventListener("click", openMenu);
+
+function openMenu() {
+  let deNav = document.querySelector("header > nav");
+  deNav.classList.add("is-open");
+}
+
+
+// 
+let sluitMenuButton = document.querySelector("header nav button");
+
+sluitMenuButton.addEventListener("click", sluitMenu);
+
+function sluitMenu() {
+  let deNav = document.querySelector("header nav");
+  deNav.classList.remove("is-open");
+}
+
