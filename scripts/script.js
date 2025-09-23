ea4e072db76e67b27c3fbabf768a52f9fb712732
Nur <aysenur1310@hotmail.com>
2025-09-23 18:05:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,43 @@ function sluitMenu() {
   deNav.classList.remove("is-open");
 }
 
+
+// footer toggle menu 1 // 
+const klapSectionInduvidu = document.querySelector("footer section:nth-of-type(1)");
+const klapButtonInduvidu = document.querySelector("footer section:nth-of-type(1) button");
+
+klapButtonInduvidu.onclick = toggleLijstjeContact;
+
+function toggleLijstjeContact() {
+  klapSectionInduvidu.classList.toggle("is-open");
+}
+
+// fotter toggle menu 2 //
+const klapSectionMiddel = document.querySelector("footer section:nth-of-type(2)");
+const klapButtonMiddel = document.querySelector("footer section:nth-of-type(2) button");
+
+klapButtonMiddel.onclick = toggleLijstjeContact2;
+
+function toggleLijstjeContact2() {
+  klapSectionMiddel.classList.toggle("is-open");
+}
+
+// fotter toggle menu 3 //
+const klapSectionOrganisatie = document.querySelector("footer section:nth-of-type(3)");
+const klapButtonOrganisatie = document.querySelector("footer section:nth-of-type(3) button");
+
+klapButtonOrganisatie.onclick = toggleLijstjeContact3;
+
+function toggleLijstjeContact3() {
+  klapSectionOrganisatie.classList.toggle("is-open");
+}
+
+// fotter toggle menu 4 //
+const klapSectionOndersteuning = document.querySelector("footer section:nth-of-type(4)");
+const klapButtonOndersteuning = document.querySelector("footer section:nth-of-type(4) button");
+
+klapButtonOndersteuning.onclick = toggleLijstjeContact4;
+
+function toggleLijstjeContact4() {
+  klapSectionOndersteuning.classList.toggle("is-open");
+}
